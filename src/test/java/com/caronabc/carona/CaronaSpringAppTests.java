dd5ca73892fa85f89df242f43ca08722f74aaf4e
abalchiumas <a.balchiumas@aluno.ufabc.edu.br>
2022-03-20 21:24:53 -0300

    Initial setup: - Spring setup - In-memory database setup

---
+++ src/test/java/com/caronabc/carona/CaronaSpringAppTests.java
@@ -0,0 +1,13 @@
+package com.caronabc.carona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronaSpringAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
