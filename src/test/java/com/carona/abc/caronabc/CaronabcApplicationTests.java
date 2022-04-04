6e2bf1606880a6483209d7c237b5c20f2a514618
abalchiumas <a.balchiumas@aluno.ufabc.edu.br>
2022-04-04 19:41:58 -0300

    Backend reworked and functional

---
+++ src/test/java/com/carona/abc/caronabc/CaronabcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carona.abc.caronabc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronabcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
