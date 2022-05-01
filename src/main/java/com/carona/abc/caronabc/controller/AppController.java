349d0b57020bf08464466dce1a01ed48e83fcaf1
abalchiumas <a.balchiumas@aluno.ufabc.edu.br>
2022-05-01 17:16:08 -0300

    Ride-share form setup (WIP)

--- src/main/java/com/carona/abc/caronabc/controller/AppController.java
+++ src/main/java/com/carona/abc/caronabc/controller/AppController.java
@@ -1,62 +1,34 @@
 package com.carona.abc.caronabc.controller;
 
-import com.carona.abc.caronabc.user.User;
-import com.carona.abc.caronabc.user.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.carona.abc.caronabc.user.UserDAO;
+import com.carona.abc.caronabc.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+@Slf4j
 public class AppController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserService userService;
 
-    @GetMapping("")
-    public ModelAndView viewHomePage() {
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
-        return modelAndView;
+    @RequestMapping
+    public ModelAndView getUsers(ModelMap modelMap) {
+        List<UserDAO> userList = userService.getUsers();
+        modelMap.addAttribute("users", userList);
+        modelMap.addAttribute("userDAO", new UserDAO());
+        return new ModelAndView("index");
     }
 
-    @GetMapping("/register")
-    public ModelAndView showRegistrationForm(Model model) {
-
-        model.addAttribute("user", new User());
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("signup_form");
-        return modelAndView;
-    }
-
-    @PostMapping("/process_register")
-    public ModelAndView processRegister(User user) {
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-
-        userRepository.save(user);
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("register_success");
-        return modelAndView;
-    }
-
-    @GetMapping("/users")
-    public ModelAndView listUsers(Model model) {
-        List<User> listUsers = userRepository.findAll();
-        model.addAttribute("listUsers", listUsers);
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("users");
-        return modelAndView;
+    @PostMapping
+    public ModelAndView createUser(ModelMap modelMap, @ModelAttribute UserDAO userDAO) {
+        UserDAO user = userService.createUser(userDAO);
+        return new ModelAndView("index");
     }
 }
