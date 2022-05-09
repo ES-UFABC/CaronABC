4ac5707235754db0d0cd9325ed9e9bdfcb7b0d6e
abalchiumas <a.balchiumas@alunmo.ufabc.edu.br>
2022-05-08 22:23:17 -0300

    Fixing app form

--- src/main/java/com/carona/abc/caronabc/controller/AppController.java
+++ src/main/java/com/carona/abc/caronabc/controller/AppController.java
@@ -3,7 +3,6 @@ package com.carona.abc.caronabc.controller;
 import com.carona.abc.caronabc.user.UserDAO;
 import com.carona.abc.caronabc.user.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,13 +11,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
-@Slf4j
+@RequestMapping
 public class AppController {
 
     private final UserService userService;
 
-    @RequestMapping
+    @GetMapping("/registro")
     public ModelAndView getUsers(ModelMap modelMap) {
         List<UserDAO> userList = userService.getUsers();
         modelMap.addAttribute("users", userList);
@@ -26,7 +24,7 @@ public class AppController {
         return new ModelAndView("index");
     }
 
-    @PostMapping
+    @PostMapping("/registro")
     public ModelAndView createUser(ModelMap modelMap, @ModelAttribute UserDAO userDAO) {
         UserDAO user = userService.createUser(userDAO);
         return new ModelAndView("index");
