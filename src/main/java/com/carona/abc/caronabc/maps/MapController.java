b8df37c80bfaa5036185f49c3a88143c131440b6
abalchiumas <a.balchiumas@aluno.ufabc.edu.br>
2022-05-12 01:10:42 -0300

    Removing unnecessary code from map controller/api

--- src/main/java/com/carona/abc/caronabc/maps/MapController.java
+++ src/main/java/com/carona/abc/caronabc/maps/MapController.java
@@ -11,32 +11,17 @@ import java.net.URISyntaxException;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api/map")
 public class MapController {
 
     private final MapService mapService;
 
-    @GetMapping("/coordinates")
-    public String generateCoordinates(@RequestParam String address) throws URISyntaxException {
-        return mapService.getGeolocation(address);
-    }
-
-    @GetMapping("/geojson")
-    public String generateGeoJson(@RequestParam String initialAddress,
-                                  @RequestParam String finalAddress) throws URISyntaxException {
-        return mapService.getDirections(initialAddress,finalAddress);
-    }
-
-    @GetMapping("/address")
-    public ModelAndView addressForm(ModelMap modelMap) {
-        modelMap.addAttribute("mapDAO", new MapDAO());
-        return new ModelAndView("address");
-    }
-
-    @PostMapping("/address")
-    public ModelAndView updateMapDirections(@ModelAttribute MapDAO mapDAO, ModelMap modelMap) throws URISyntaxException {
+    @RequestMapping("/address/{initialAddress}/{finalAddress}")
+    public ModelAndView addressForm(ModelMap modelMap,
+                                    @PathVariable(value = "initialAddress") String initialAddress,
+                                    @PathVariable(value = "finalAddress") String finalAddress) throws URISyntaxException {
         modelMap.addAttribute("geoJsonDirections",
-                mapService.getDirections(mapDAO.getInitialAddress(),mapDAO.getFinalAddress()));
+                mapService.getDirections(initialAddress,finalAddress));
+
         return new ModelAndView("map");
     }
 }
