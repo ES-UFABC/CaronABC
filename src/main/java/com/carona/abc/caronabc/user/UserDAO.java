2cb98a8236adfc33aabd5d24a2175492d5acedd5
abalchiumas <a.balchiumas@aluno.ufabc.edu.br>
2022-05-12 01:07:34 -0300

    Updating in-memory database

--- src/main/java/com/carona/abc/caronabc/user/UserDAO.java
+++ src/main/java/com/carona/abc/caronabc/user/UserDAO.java
@@ -1,25 +1,44 @@
 package com.carona.abc.caronabc.user;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Date;
 
-@Entity
 @Data
+@Entity
+@Table(name = "users")
 public class UserDAO {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column
     private String fullName;
+
+    @Column
     private String initialAddress;
+
+    @Column
     private String finalAddress;
+
+    @Column
     private String email;
+
+    @Column
     private String phoneNumber;
-    private boolean active = true;
+
+    @Column
+    //@DateTimeFormat(pattern = "dd/MM/YYYY HH:mm")
+    // TODO: should be treated as Date instead of String
+    private String departureTime;
+
+    @Column
+    private Ride ride;
+
+    @Column
     private LocalDateTime requestedAt = LocalDateTime.now();
 }
